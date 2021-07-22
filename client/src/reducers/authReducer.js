7955fde2767e17f2805c8450dbfda0d728e7dfcc
Varun Kumar <heysadboy@gmail.com>
2021-07-22 22:17:27 +0900

    updating reducers

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -8,9 +8,9 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case SIGN_IN:
-            return { ...state, isSignedIn: true, usedId: action.payload };
+            return { ...state, isSignedIn: true, userId: action.payload };
         case SIGN_OUT:
-            return { ...state, isSignedIn: false,  usedId: null  };
+            return { ...state, isSignedIn: false,  userId: null  };
         default:
             return state;
     }
