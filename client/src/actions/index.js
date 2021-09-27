c52376c26f20a581f27f6174b1d52ce64ffc0c6c
Varun Kumar <heysadboy@gmail.com>
2021-09-27 22:41:14 +0900

    adding

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,6 +8,7 @@ import {
     EDIT_STREAM
 } from './types';
 import streams from '../apis/streams';
+import history from '../history';
 import { formValues } from 'redux-form';
 
 export const signIn = (userId) => {
@@ -29,6 +30,7 @@ export const createStream = formValues => async (dispatch, getState) => {
     const response = await streams.post('/streams', { ...formValues, userId });
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
+    history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
